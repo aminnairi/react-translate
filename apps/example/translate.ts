be3e69313887eff3db702358e29678d957d1eb53
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-08 23:06:51 +0100

    added an example for the local storage adapter

--- apps/example/translate.ts
+++ apps/example/translate.ts
@@ -1,9 +1,10 @@
-import { createTranslations } from "@aminnairi/react-translate";
+import { createTranslations, createLocalStorageAdapter } from "@aminnairi/react-translate";
 
 export const { LocaleProvider, useTranslate, useLocale, defineTranslation } = createTranslations({
   initialLocale: "en",
   locales: [
-    "fr",
-    "en"
-  ]
+    "en",
+    "fr"
+  ],
+  storage: createLocalStorageAdapter("locale"),
 });
